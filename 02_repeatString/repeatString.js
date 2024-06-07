b78232a334cd2a2b50f0768bc6a060e411bc4f19
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-07 20:12:20 +0530

    ADD LINES OF CODES TO REPEAT STRINGS 'N' NUMBER OF TIMES

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str1 , num1) {
+    let str = '';
+    if(num1>=0){
+        for(let i=0;i<num1;i++){
+            str += str1;
+        }
+    }else{
+        str += 'ERROR';
+    }
+    return str;
 };
 
 // Do not edit below this line
