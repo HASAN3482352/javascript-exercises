272b5d9c915995082f8d8f4cf53335f479642ae1
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-10 18:29:30 +0530

    COMPLETES THE FUNCTION TO REMOVE ELEMENTS FROM THE PROVIDED ARRAY

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let newArr=[],x=0; 
+    for(let i=0;i<arr.length;i++){
+        x=0;
+        for(let j=0;j<args.length;j++){
+            if(arr[i]!==args[j]){
+                x++;
+            }
+        }
+        if(x==args.length){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+
 };
 
 // Do not edit below this line
