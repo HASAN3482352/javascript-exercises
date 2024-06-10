b9630b63290800ce6cbc211e2f7cd12fa75ca37f
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-10 18:45:04 +0530

    COMPLETES THE FUNCTION TO RETURN THE SUM OF INTEGERS PROVIDED AS ARGUMENT AND THE INTEGERS BETWEEN THEM

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sum=0;
+    if(start>end){
+        let temp=start;
+        start=end;
+        end=temp;
+    }
+    if(start<0 || end<0){
+        return 'ERROR';
+    }else if(typeof(start)=='string' || typeof(end)=='string'){
+        return 'ERROR';
+    }else if(typeof(start)=='object' || typeof(end)=='object'){
+        return 'ERROR';
+    }
+    for(let i=start;i<=end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
