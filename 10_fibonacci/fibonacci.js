f27546a1ed42ec1c41482425d4beecaa9c19238b
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-16 06:29:02 +0530

    COMPLETE THE FUNCTION TO RETURN THE NTH FIBONACCI NUMBER

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(+num==0){
+        return 0;
+    }else if(+num<0){
+        return 'OOPS';
+    }
+    let result=[1,1];
+    for(let i=2;i<+num;i++){
+        result[i]=result[i-1]+result[i-2];        
+    }
+    return result[+num-1];
 };
 
 // Do not edit below this line
