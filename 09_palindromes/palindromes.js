d0d2ffebdb7609fd65521ca055ea4dfe927177cc
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-16 06:08:44 +0530

    COMPLETE THE PALINDROME FUNCTION

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const allowedChar = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const cleanedString = string.toLowerCase()
+                                .split('')
+                                .filter(string=>allowedChar.includes(string))
+                                .join('');
+    const reversedStr = cleanedString.split('')
+                                    .reverse()
+                                    .join('');
+    return cleanedString===reversedStr;
 };
 
 // Do not edit below this line
