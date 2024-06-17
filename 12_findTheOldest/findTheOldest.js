08b84915392ff7782724cd90195dc940f834701e
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-18 02:33:04 +0530

    COMPLETE THE FUNCTION TO FIND THE OLDEST PERSON

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.reduce((age,person)=>{
+        if(person.yearOfDeath===undefined){
+            person.yearOfDeath=new Date().getFullYear();
+        }
+        if((person.yearOfDeath-person.yearOfBirth)>age.oldest){
+            age.oldest=person.yearOfDeath-person.yearOfBirth;
+            age.name=person.name;
+        }
+        return age;
+    },{
+        oldest:0,
+    });
+    return oldest;
 };
 
 // Do not edit below this line
